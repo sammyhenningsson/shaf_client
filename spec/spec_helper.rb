c4ae281324e590266149ac8cf7a6239eddd4a8cb
Sammy Henningsson <sammy.henningsson@gmail.com>
2021-01-24 08:16:16 +0100

    Move stubbing logic into specs only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 require 'shaf_client'
 require 'minitest/autorun'
+
+module Stubbing
+  def stubs
+    return unless @adapter == :test
+    @stubs ||= Faraday::Adapter::Test::Stubs.new
+  end
+
+  def adapter_args
+    args = super
+    args << stubs if @adapter == :test
+    args 
+  end
+end
+
+ShafClient.prepend Stubbing
